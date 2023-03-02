ef3fa70b4a9070f5c702ecb48ffa30889219fd70
LilNoahChan <noahchan305@gmail.com>
2023-03-01 22:46:59 -0500

    s

--- create-task/js/dom.js
+++ create-task/js/dom.js
@@ -4,7 +4,7 @@ const DOMSelectors = {
     button3: document.getElementById("b3"), //vitamins
     button4: document.getElementById("b4"), //household
     button5: document.getElementById("b5"), //personal
-    box: document.getElementById("box"),
+    box: document.getElementById("container"),
 };
 
 export { DOMSelectors };
--- create-task/js/main.js
+++ create-task/js/main.js
@@ -35,6 +35,91 @@ const commands = {
     });
   },
 
+  FilterGrocery: function () {
+    items.filter((items)=> items.section.includes("grocery"))
+    .forEach((items)=> {
+      DOMSelectors.box.insertAdjacentElement(
+      "beforeend",
+      `
+              <div class="inner">
+              <h2>${items.itemName}</h2>
+              <p><img class="picture" src="${items.img}" alt="picture of ${items.img}"> a</p>
+              <p>Category: ${items.section}</p>
+              <p>Price: ${items.itemCost}</p>
+              </div>
+            `
+      );
+    });
+  },
+
+  FilterVitamins: function () {
+    items.filter((items)=> items.section.includes("vitamins"))
+    .forEach((items)=> {
+      DOMSelectors.box.insertAdjacentElement(
+      "beforeend",
+      `
+              <div class="inner">
+              <h2>${items.itemName}</h2>
+              <p><img class="picture" src="${items.img}" alt="picture of ${items.img}"> a</p>
+              <p>Category: ${items.section}</p>
+              <p>Price: ${items.itemCost}</p>
+              </div>
+            `
+      );
+    });
+  },
+
+  FilterHousehold: function () {
+    items.filter((items)=> items.section.includes("household"))
+    .forEach((items)=> {
+      DOMSelectors.box.insertAdjacentElement(
+      "beforeend",
+      `
+              <div class="inner">
+              <h2>${items.itemName}</h2>
+              <p><img class="picture" src="${items.img}" alt="picture of ${items.img}"> a</p>
+              <p>Category: ${items.section}</p>
+              <p>Price: ${items.itemCost}</p>
+              </div>
+            `
+      );
+    });
+  },
+
+  FilterHousehold: function () {
+    items.filter((items)=> items.section.includes("household"))
+    .forEach((items)=> {
+      DOMSelectors.box.insertAdjacentElement(
+      "beforeend",
+      `
+              <div class="inner">
+              <h2>${items.itemName}</h2>
+              <p><img class="picture" src="${items.img}" alt="picture of ${items.img}"> a</p>
+              <p>Category: ${items.section}</p>
+              <p>Price: ${items.itemCost}</p>
+              </div>
+            `
+      );
+    });
+  },
+
+  FilterPersonal: function () {
+    items.filter((items)=> items.section.includes("personal"))
+    .forEach((items)=> {
+      DOMSelectors.box.insertAdjacentElement(
+      "beforeend",
+      `
+              <div class="inner">
+              <h2>${items.itemName}</h2>
+              <p><img class="picture" src="${items.img}" alt="picture of ${items.img}"> a</p>
+              <p>Category: ${items.section}</p>
+              <p>Price: ${items.itemCost}</p>
+              </div>
+            `
+      );
+    });
+  },
+  
   remove: function () {
     let card = document.querySelectorAll(".inner");
     card.forEach((card) => {
@@ -48,5 +133,26 @@ commands.createcards();
 //everything
 DOMSelectors.button.addEventListener("click", function (){
   commands.remove();
-  commands.FilterAll()
-})
+  commands.FilterAll();
+});
+
+//grocery
+DOMSelectors.button2.addEventListener("click", function (){
+  commands.remove();
+  commands.FilterGrocery();
+});
+
+DOMSelectors.button3.addEventListener("click", function (){
+  commands.remove();
+  commands.FilterVitamins();
+});
+
+DOMSelectors.button4.addEventListener("click", function (){
+  commands.remove();
+  commands.FilterHousehold();
+});
+
+DOMSelectors.button5.addEventListener("click", function (){
+  commands.remove();
+  commands.FilterPersonal();
+});
