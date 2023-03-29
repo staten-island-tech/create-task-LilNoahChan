dfdcf64c953ae5caac42ee27411ca95f8bcefc8d
LilNoahChan <noahchan305@gmail.com>
2023-03-28 22:02:47 -0400

    a

--- create-task/js/dom.js
+++ create-task/js/dom.js
@@ -1,9 +1,9 @@
 const DOMSelectors = {
-    button: document.getElementById("b1"), //everything
-    button2: document.getElementById("b2"), //grocery
-    button3: document.getElementById("b3"), //vitamins
-    button4: document.getElementById("b4"), //household
-    button5: document.getElementById("b5"), //personal
+    button: document.getElementById("b1"), 
+    button2: document.getElementById("b2"), 
+    button3: document.getElementById("b3"),
+    button4: document.getElementById("b4"), 
+    button5: document.getElementById("b5"), 
     box: document.getElementById("items"),
     list: document.getElementById("theItems"),
     btn: document.getElementById("submit"),
